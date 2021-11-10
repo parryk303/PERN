5467065318374ea585093dc2f4f6ddc6bcfad124
Kyle Parry <kyle.parry.303@gmail.com>
2021-11-10 13:50:40 -0700

    Add hooks frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,48 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
-import React from 'react';
+import React, { useState, useEffect } from 'react'
 import axios from 'axios';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { apiResponse: [] };
+const App = () => {
+  const [posts, setPosts] = useState()
+
+  const getData = async () => {
+    await axios.get('https://jsonplaceholder.typicode.com/posts')
+    .then(res => setPosts(res.data))
   }
 
-  callAPI() {
-    axios.get('https://jsonplaceholder.typicode.com/posts')
-      .then(res => {
-        console.log(res.data)
-        this.setState({ apiResponse: res.data })
-      });
-  }
+  useEffect(() => {
+    getData()
+  }, [])
 
-  componentDidMount() {
-    this.callAPI();
-  }
-
-  render() {
-    return (
-      <div className='App'>
-        <header className='App-header'>
-          <img src={logo} className='App-logo' alt='logo' />
-          {this.state.apiResponse.map((post) => (
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        {/* check if posts array is defined first before mapping, allows time for async */}
+        {posts && posts.map((post) => (
           <p className='App-intro'>
-            Response: {post.body}
+            Response: {post.id}
           </p>
-          ))}
-          <a
-            className='App-link'
-            href='https://reactjs.org'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+        ))}
+        <a
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+
 }
 
 export default App;
